4e5663314610ca3a2ce32af91fb33a0afe79acb3
(대학원생 기계공학과(공과대학)) CLINT ALEX STEED <steed@unist.ac.kr>
2021-06-03 10:54:54 +0900

    Mover working in static tests.

---
+++ Top Down explorer/Assets/UnityMover/PlayTests/RecieverTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
+using UnityMover;
+
+namespace Tests
+{
+    public class RecieverTest
+    {
+        private Receiver receiver;
+        private string moveMessage = "Move:(1, 2, 3)";
+        private string moveWithSpace = "Move : (1, 2, 3)";
+        private Vector3 destination = new Vector3(1, 2, 3);
+        private TeleportMover teleportMover;
+        
+        [SetUp]
+        public void CreateNewReciever()
+        {
+            teleportMover = new TeleportMover();
+            receiver = new Receiver(teleportMover);
+        }
+
+        [TearDown]
+        public void RemoveReciever()
+        {
+            receiver = null;
+        }
+
+        [Test]
+        public void RecieverNotNull()
+        {
+            Assert.IsNotNull(receiver);
+        }
+
+        [Test]
+        public void RecieverMessageNotUnderstood()
+        {
+            Assert.Catch<Receiver.RecieverMessageNotUnderstood>(() => receiver.Receive("bogus message"));
+        }
+
+        [Test]
+        public void MoveShouldNotThrowException()
+        {
+            receiver.Receive(moveMessage);
+        }
+
+        [Test]
+        public void MoveWithSpaceShouldNotThrowException()
+        {
+            receiver.Receive(moveWithSpace);
+        }
+
+
+        [Test] public void InitialMoverPositionIsZero()
+        {
+            Assert.AreEqual(teleportMover.location, Vector3.zero);
+        }
+        
+        [Test]
+        public void ShouldMoveToPosition()
+        {
+            Moveable moveable = receiver.Receive(moveMessage);
+            moveable.MoveTo();
+            Assert.AreNotEqual(teleportMover.location, Vector3.zero,"Should have moved");
+            Assert.AreEqual(teleportMover.location, destination, "Should be at destination"); 
+        }
+
+        [Test]
+        public void StringEquals()
+        {
+            Assert.IsTrue("a".Equals("a"));
+            Assert.IsTrue("a" == "a");
+            Assert.IsFalse("a".Equals("b"));
+            Assert.IsFalse("A".Equals("a"));
+        }
+        
+    }
+
+    internal class TeleportMover: Moveable
+    {
+        public Vector3 location = Vector3.zero;
+        private Vector3 mydestination;
+        public void SetDestination(Vector3 destination)
+        {
+            mydestination = destination;
+        }
+
+        public void MoveTo()
+        {
+            location = mydestination;
+        }
+    }
+}
---
+++ Top Down explorer/Assets/UnityMover/Receiver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityMover
+{
+    public class Receiver
+    {
+        private Moveable moveable;
+
+        public Receiver(Moveable moveable)
+        {
+            this.moveable = moveable;
+        }
+
+        public Moveable Receive(string instruction)
+        {
+            return GetCommand(instruction);
+        }
+
+        private Moveable GetCommand(string instruction)
+        {
+            string[] split = instruction.Replace(" ", "").Trim(' ').Split(':');
+            if (split[0] == "Move")
+            {
+                string coordinates = split[1];
+            
+                Vector3 destination = ToVector3(coordinates);
+                moveable.SetDestination(destination);
+                return moveable;
+            }
+            else
+            {
+                throw new RecieverMessageNotUnderstood();
+            }
+        }
+
+        private static Vector3 ToVector3(string coordinates)
+        {
+            string[] coords = coordinates.Trim('(').Trim(')').Split(',');
+
+            Vector3 output = new Vector3(float.Parse(coords[0]), float.Parse(coords[1]), float.Parse(coords[2]));
+            return output;
+
+        }
+
+        public class RecieverMessageNotUnderstood : Exception
+        {
+        }
+    }
+
+    public interface Moveable
+    {
+        void SetDestination(Vector3 destination);
+        void MoveTo();
+    }
+
+    
+}
